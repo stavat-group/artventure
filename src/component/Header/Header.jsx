d794c477fd3726ce06cd1d5ab8eca0ca42246c73
thevasudev <vasudev082002@gmail.com>
2025-02-26 18:00:57 +0530

    changes 2

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
 import "./Header.css";
-
+import artventurebg from "../../assets/artventurebg.png";
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className="header">
-      <div className="logo">LOGO</div>
+      <div className="logo">
+        <img src={artventurebg} alt="Artventure Logo"
+        style={{
+          width: "200px",
+        }}
+        />
+      </div>
 
-      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+      {/* <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
         ☰
-      </button>
+      </button> */}
 
       <nav className={`nav ${menuOpen ? "open" : ""}`}>
-        <ul className="nav-links">
+        {/* <ul className="nav-links">
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/faqs">FAQs</a></li>
           <li><a href="/contact">Contact</a></li>
-          
-        </ul>
+
+        </ul> */}
       </nav>
     </header>
   );
